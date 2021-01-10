9f73aa113c5abb58f40d41e86a1771464eff3cda
bonnietsang-octopus <68524141+bonnietsang-octopus@users.noreply.github.com>
2021-01-10 17:36:48 +1000

    ex 1 - useReducer

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -1,18 +1,35 @@
 // useReducer: simple Counter
 // http://localhost:3000/isolated/exercise/01.js
 
-import * as React from 'react'
+import * as React from 'react';
+
+
+const countReducer = (state, action) => {
+
+  switch(action.type) {
+    case "INCREMENT" : {
+      return {count: state.count + action.step}
+    }
+    default: {
+      throw new Error(`Unsupported action type: ${action.type}`);
+    }
+  }
+
+
+
+  // ...state,
+  // ...(typeof action === "function" ? action(state) : action),
+}
 
 function Counter({initialCount = 0, step = 1}) {
-  // 🐨 replace React.useState with React.useReducer.
-  // 💰 React.useReducer(countReducer, initialCount)
-  const [count, setCount] = React.useState(initialCount)
 
-  // 💰 you can write the countReducer function so you don't have to make any
-  // changes to the next two lines of code! Remember:
-  // The 1st argument is called "state" - the current value of count
-  // The 2nd argument is called "newState" - the value passed to setCount
-  const increment = () => setCount(count + step)
+  const [state, dispatch] = React.useReducer(countReducer, {
+    count: initialCount,
+  });
+
+  const {count} = state;
+  const increment = () => dispatch({type: "INCREMENT", step});
+  // const increment = () => setState(currentState => ({count: currentState.count + step}));
   return <button onClick={increment}>{count}</button>
 }
 
@@ -21,3 +38,147 @@ function App() {
 }
 
 export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // useReducer: simple Counter
+// // http://localhost:3000/isolated/exercise/01.js
+
+// import * as React from 'react';
+// import { useState, useReducer } from 'react';
+
+
+// // type CounterProps = {
+// //   initialCount?: number,
+// //   step?: number,
+// // };
+
+
+// // type CounterState = {
+// //   count: number,
+// //   label: string,
+// // };
+
+
+// /**
+//  * REDUCER
+//  */
+// const stateReducer = (prevState, action) => {
+//   let result = prevState;
+
+//   switch(action.type){
+//     case 'count': 
+//       result = {
+//         ...prevState,
+//         count: prevState.count + action.step,
+//       };
+//       break;
+
+//     case 'label': 
+//       result = {
+//         ...prevState,
+//         label: action.label,
+//       };
+//       break;
+
+//     default:
+//       result = prevState;
+//   }
+
+//   return result;
+// };
+
+
+// /**
+//  * COMPONENT
+//  */
+// const Counter = function Counter(props) {
+//   const {
+//     initialCount = 0,
+//     step = 1,
+//   } = props;
+
+//   const [oldState, setOldState] = useState({
+//     count: initialCount,
+//     label: 'My label',
+//   });
+
+//   const [textValue, setTextValue] = useState('My label');
+
+//   const [state, setState] = useReducer(stateReducer, {
+//     count: initialCount,
+//     label: 'My label',
+//   });
+
+
+
+
+//   const { count } = state;
+//   const handleClick = () => {
+
+//     setOldState((prevState) => {
+//       const nextCount = {
+//         count: count + step,
+//       };
+
+//       const newState = {
+//         ...prevState,
+//         ...nextCount,
+//       };
+//       return newState;
+//     });
+
+
+//     setState({
+//       type: 'count',
+//       step: 2,
+//     });
+
+//   };
+
+//   const handleClick2 = () => {
+//     setState({
+//       type: 'label',
+//       label: textValue,
+//     });
+//   }
+
+//   return (
+//     <>
+//       <div>
+//         <button onClick={handleClick}>{ state.label }: { count }</button>
+//       </div>
+
+//       <div>
+//         <input type="text" 
+//           value={textValue}
+//           onChange={e => setTextValue(e.currentTarget.value)}
+//         />
+//         <button onClick={handleClick2}>Set label</button>
+
+//       </div>
+//     </>
+//   );
+// }
+
+// function App() {
+//   return <Counter />
+// }
+
+// export default App
